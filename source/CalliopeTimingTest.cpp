e68bae551b29bf28e286645bd8481debce525839
Matthias L. Jugel <leo@thinkberg.com>
2016-09-23 11:25:42 +0200

    minimize test

---
+++ source/CalliopeTimingTest.cpp
@@ -0,0 +1,20 @@
+#include "MicroBit.h" 
+#include "CalliopeRGB.h" 
+#include "CalliopeSoundMotor.h" 
+
+MicroBit uBit;
+CalliopeRGB                 rgb;
+
+int main() {
+    uBit.init();
+
+    rgb.Set_Color(0xff, 0x00, 0x00, 0);
+    uBit.sleep(1000);
+    rgb.Set_Color(0x00, 0xff, 0x00, 0);
+    uBit.sleep(1000);
+    rgb.Set_Color(0x00, 0x00, 0xff, 0);
+    uBit.sleep(1000);
+    rgb.Set_Color(0xff, 0xff, 0xff, 0);
+
+} 
+
