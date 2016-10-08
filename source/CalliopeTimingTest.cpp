2ce1f1b771e9f0b6997e85eb78352074893dd1a8
Matthias L. Jugel <leo@thinkberg.com>
2016-10-08 12:23:29 +0200

    bump pxt-calliope-core dependency

--- source/CalliopeTimingTest.cpp
+++ source/CalliopeTimingTest.cpp
@@ -1,20 +1,28 @@
-#include "MicroBit.h" 
-#include "CalliopeRGB.h" 
+#include "MicroBitSamples.h"
+#include "MicroBit.h"
+#include "CalliopeRGB.h"
+
+#ifdef MICROBIT_SAMPLE_RGB_DEMO
 
 MicroBit uBit;
-CalliopeRGB                 rgb;
-MicroBitDisplay display;
 
 int main() {
     uBit.init();
+    uBit.serial.baud(115200);
+    uBit.serial.send("RGB DEMO\n\r");
 
-    display.scroll("RED");
-    rgb.Set_Color(0xff, 0x00, 0x00, 0);
-    display.scroll("GREEN");
-    rgb.Set_Color(0x00, 0xff, 0x00, 0);
-    display.scroll("BLUE");
-    rgb.Set_Color(0x00, 0x00, 0xff, 0);
-    display.scroll("OFF");
-    rgb.Set_Color(0xff, 0xff, 0xff, 0);
+    uBit.display.scroll("RED");
+    uBit.serial.send("RED\n\r");
+    uBit.rgb.Set_Color(0xff, 0x00, 0x00, 0);
+    uBit.display.scroll("GREEN");
+    uBit.serial.send("GREEN\n\r");
+    uBit.rgb.Set_Color(0x00, 0xff, 0x00, 0);
+    uBit.display.scroll("BLUE");
+    uBit.serial.send("BLUE\n\r");
+    uBit.rgb.Set_Color(0x00, 0x00, 0xff, 0);
+    uBit.display.scroll("OFF");
+    uBit.serial.send("OFF\n\r");
+    uBit.rgb.Set_Color(0xff, 0xff, 0xff, 0);
 } 
 
+#endif
